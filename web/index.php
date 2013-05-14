abdfdcc68f13b77a1d170cc87c6be4f00e11e2b2
OGRE7 <paulin.trognon@gmail.com>
2013-05-14 13:31:28 +0200

    adding htaccess + index file

---
+++ web/index.php
@@ -0,0 +1,20 @@
+<?php
+
+//On initialise le timeZone
+ini_set('date.timezone', 'Europe/Brussels');
+
+//On ajoute l'autoloader
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
+
+//dans l'autoloader nous ajoutons notre répertoire applicatif 
+$loader->add("App",dirname(__DIR__));
+
+
+//Nous instancions un objet Silex\Application
+$app = new Silex\Application();
+
+//en dev, nous voulons voir les erreurs
+$app['debug'] = true;
+
+//On lance l'application
+$app->run();
